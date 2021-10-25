a33d47c75871c7785b7bdd0ffc9a494f693b1e6e
syed <syed@email.com>
2021-10-25 08:52:12 +0530

    1000

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 var a= 80
 var b= 80
-var c= 90
-=======
-var a =80
-var b = 80
-var c = 90
-var d = 20
->>>>>>> feature-1
-=======
-var a= 80
-var b= 80
-var c= 90
-var d= 20
->>>>>>> feature-1
+var c= 100
